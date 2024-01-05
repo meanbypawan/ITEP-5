c44c6b5f5ec3509014cffb3388ff3a64332949b7
mean <meanstack4@gmail.com>
2024-01-05 11:28:24 +0530

    http update

--- js/nodejs/testhttp/server1.js
+++ js/nodejs/testhttp/server1.js
@@ -1,14 +1,24 @@
 import http from "http";
+import url from "url";
 
 const server = http.createServer((request,response)=>{
-    if((request.url == "/home" || request.url == '/') && request.method == "GET"){
+    //console.log(request.url);
+    let parsedUrl = url.parse(request.url,true);
+    console.log(parsedUrl);
+    if((parsedUrl.pathname == "/home" || parsedUrl.pathname.url == '/') && request.method == "GET"){
         response.write("Home Page");
     }
-    else if((request.url == '/about') && request.method=="GET"){
+    else if((parsedUrl.pathname == '/about') && request.method=="GET"){
         response.write("About Page");
     }      
-    else if(request.url == '/contact' && request.method=="GET"){
+    else if(parsedUrl.pathname == '/contact' && request.method=="GET"){
         response.write("Contact Page");
+    }
+    else if(parsedUrl.pathname == "/add"){
+        let a = parsedUrl.query.a*1;
+        let b = parsedUrl.query.b*1;
+        
+        response.write("<h1>Addition : "+(a+b)+"</h1>");
     }
     response.end();
 });
