d379d540123c86fe243c78b973f674ba89b6240e
mean <meanstack4@gmail.com>
2024-01-09 11:12:01 +0530

    mysql connectivity

--- js/nodejs/test-express/app.js
+++ js/nodejs/test-express/app.js
@@ -1,9 +1,8 @@
 import express from "express";
 import bodyParser from 'body-parser';
+import pool from './dbConfig.js';
+
 const app = express();
-
-// http://localhost:3000/contact
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -28,9 +27,44 @@ app.post("/signup",(request,response,next)=>{
     let password = request.body.password;
     let email = request.body.email;
     let age = request.body.age;
-    // write code to save data in database........
-    return response.json({data: {username,email,age}});
-})
+    
+    pool.getConnection((err,con)=>{
+        if(!err){
+          let sql = "insert into user(username,email,password,age) values(?,?,?,?)";
+          con.query(sql,[username,email,password,age*1],(err,result)=>{
+            if(err)
+              return response.status(401).json({error: 'Bad request'});
+            else
+              return response.status(200).json({message: 'Signup success', data:{username,email,age}});
+            con.release();
+          });
+        }
+        else{
+           console.log("Connection Failure...");
+           console.log(err);
+        }
+    })
+});
+app.post("/signin",(request,response,next)=>{
+   let email = request.body.email;
+   let password = request.body.password;
+
+   pool.getConnection((err,con)=>{
+    if(!err){
+      let sql = "select * from user where email = ? and password = ?";
+      con.query(sql,[email,password],(err,result)=>{
+        if(err)
+          return response.status(500).json({error: 'Internal Server Error'});
+        else if(result.length!=0)
+              return response.status(200).json({message: 'Sign in success...'});
+             else
+              return response.status(401).json({error: "Unauthorized request..."}); 
+      });
+    }
+    else
+      return response.status(500).json({error: 'Internal Server Error'});
+   })
+});
 
 app.post("/add",(request,response,next)=>{
     let a  = request.body.a*1;
