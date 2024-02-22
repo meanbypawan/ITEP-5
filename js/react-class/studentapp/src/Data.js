07956734580fe2a44c017fa7db21b29aa6f73b4d
mean <meanstack4@gmail.com>
2024-02-22 16:13:53 +0530

    student app

---
+++ js/react-class/studentapp/src/Data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        roll: 1001,
+        name: 'Dharna',
+        branch: "CS",
+        mobile: 9009111222
+    },
+    {
+        roll: 1002,
+        name: 'Priyanshi',
+        branch: "IT",
+        mobile: 9009111333
+    },
+    {
+        roll: 1003,
+        name: 'Cheeku',
+        branch: "EC",
+        mobile: 9009111333
+    },
+    {
+        roll: 1004,
+        name: 'Peeku',
+        branch: "MECH",
+        mobile: 9009111444
+    },
+    {
+        roll: 1005,
+        name: 'Sontu',
+        branch: "CS",
+        mobile: 9009111555
+    }
+]
